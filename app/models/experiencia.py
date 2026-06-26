7f06dd7a469504056ecde6c099e3b1f236d5970a
Eduardo Arnold Cavalheiro <eduarnoldcvalheiro@gmail.com>
2026-06-26 00:40:30 -0300

    campos de imagem nas tabelas de hoteis e experiencia

--- app/models/experiencia.py
+++ app/models/experiencia.py
@@ -8,3 +8,5 @@ class Experiencia(db.Model):
     cidade_id = db.Column(db.Integer, db.ForeignKey('cidade.id'))
 
     categoria = db.Column(db.String(50))
+    
+    imagem = db.Column(db.String(300), nullable=True)
--- app/models/hotel.py
+++ app/models/hotel.py
@@ -9,5 +9,6 @@ class Hotel(db.Model):
 
     endereco = db.Column(db.String(200))
     telefone = db.Column(db.String(20))
+    imagem = db.Column(db.String(300), nullable=True)
 
     acessibilidade = db.relationship('Acessibilidade', backref='hotel', uselist=False)
